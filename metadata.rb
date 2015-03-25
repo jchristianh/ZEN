d79be2144dc380849123d009ba5d83599f664e5e
Chris Hammer <chris@thezengarden.net>
2015-03-25 19:24:23 -0400

    Updated license for public release

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'ZEN'
-maintainer       'Copyright (C) 2015 The Zen Garden. All rights reserved.'
+maintainer       'Copyright (C) 2015 Chris Hammer'
 maintainer_email 'chris@thezengarden.net'
-license          'All rights reserved'
+license          'GPL v2'
 description      'Centralized package installs for all of our cookbooks.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.5.0'
